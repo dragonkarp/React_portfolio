b870052f1bc4e468ed228c0a4844e4261d759937
Michael Li <dev.mike011235@gmail.com>
2020-09-30 19:18:39 -0400

    first commit

---
+++ src/components/BottomBar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import { Button, Navbar, Form} from "react-bootstrap"
+
+function BottomBar() {
+    return (
+        <div>
+            <p>Michael Li</p>
+        </div>
+  );
+}
+
+export default BottomBar;
---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import TopNavbar from "./components/TopNavbar"
+import BottomBar from "./components/BottomBar"
+
+
+function Homepage() {
+  return (
+    <div>
+        <TopNavbar />
+        <BottomBar />
+    </div>
+  );
+}
+
+export default Homepage;
